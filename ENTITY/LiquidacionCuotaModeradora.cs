b066c72d2c3d689b96e3913e7f9c171454a9b615
HERNAN GUTIERREZ <hernanandres.haga@gmail.com>
2023-09-22 09:48:46 -0500

    Modificacion capa entidad

---
+++ ENTITY/LiquidacionCuotaModeradora.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY
+{
+    public class LiquidacionCuotaModeradora
+    {
+        public int numeroLiquidacion {  get; set; }
+        public int idPaciente { get; set; } 
+        public int tipoAfilacion { get; set; }
+        public Double salarioDevengado { get; set; }    
+        public Double valorHospitalizacion { get; set; }    
+        public Double valorCuotaModeradora { get; set; }
+
+        public LiquidacionCuotaModeradora(int numeroLiquidacion, int idPaciente, int tipoAfilacion, double salarioDevengado, double valorHospitalizacion)
+        {
+            this.numeroLiquidacion = numeroLiquidacion;
+            this.idPaciente = idPaciente;
+            this.tipoAfilacion = tipoAfilacion;
+            this.salarioDevengado = salarioDevengado;
+            this.valorHospitalizacion = valorHospitalizacion;
+        }
+
+
+
+
+
+    }
+}
--- Presentacion/Presentacion.cs
+++ Presentacion/Presentacion.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Presentacion
 {
-    internal class Program
+    internal class Presentacion
     {
         static void Main(string[] args)
         {
