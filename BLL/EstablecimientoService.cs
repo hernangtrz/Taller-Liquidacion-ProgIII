d51d6d84afa739478942153297c1d832f4d66826
HERNAN GUTIERREZ <hernanandres.haga@gmail.com>
2023-09-21 20:47:03 -0500

    Primer commit

---
+++ BLL/EstablecimientoService.cs
@@ -0,0 +1,98 @@
+﻿using DAL;
+using ENTITY;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class EstablecimientoService
+    {
+        private readonly EstablecimientoRepository establecimientoRepository;
+        public EstablecimientoService()
+        {
+            establecimientoRepository = new EstablecimientoRepository();
+        }
+
+        public Double CalcularGanacias(Double valorIngresosAnuales, Double valorGastosAnuales)
+        {
+            return valorIngresosAnuales - valorGastosAnuales;
+        }
+
+        public Double PasarUvt(Double gananciasPesos)
+        {
+
+            return gananciasPesos / 25000;
+        }
+        public string Guardar(Establecimiento establecimiento)
+        {
+            try
+            {
+
+                if (establecimientoRepository.Buscar(establecimiento.identificacion) == null)
+                {
+                    establecimientoRepository.Guardar(establecimiento);
+                    return $"se han guardado Satisfactoriamente los datos del establecimiento: {establecimiento.nombre} ";
+                }
+                else
+                {
+                    return $"Lo sentimos, con la Identificación {establecimiento.identificacion} ya se encuentra registrada";
+                }
+            }
+            catch (Exception e)
+            {
+
+                return $"Error de la Aplicacion: {e.Message}";
+            }
+        }
+        public string Eliminar(string identificacion)
+        {
+            try
+            {
+                if (establecimientoRepository.Buscar(identificacion) != null)
+                {
+                    establecimientoRepository.Eliminar(identificacion);
+                    return ($"se han Eliminado Satisfactoriamente los datos del establecimiento con Identificación: {identificacion} ");
+                }
+                else
+                {
+                    return ($"Lo sentimos, no se encuentra registrada un establecimiento con Identificacion {identificacion}");
+                }
+            }
+            catch (Exception e)
+            {
+
+                return $"Error de la Aplicacion: {e.Message}";
+            }
+
+        }
+        public List<Establecimiento> ConsultarTodos()
+        {
+                List<Establecimiento> establecimientos = establecimientoRepository.ConsultarTodos();
+                return establecimientos;
+        }
+
+        public class ConsultaEstablecimientoResponse
+        {
+            public List<Establecimiento> Establecimientos { get; set; }
+            public string Message { get; set; }
+            public bool Encontrado { get; set; }
+
+            public ConsultaEstablecimientoResponse(List<Establecimiento> establecimientos)
+            {
+                Establecimientos = new List<Establecimiento>();
+                Establecimientos = establecimientos;
+                Encontrado = true;
+            }
+            public ConsultaEstablecimientoResponse(string message)
+            {
+                Message = message;
+                Encontrado = false;
+            }
+        }
+
+
+    }
+}
---
+++ DAL/EstablecimientoRepository.cs
@@ -0,0 +1,86 @@
+﻿using ENTITY;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class EstablecimientoRepository
+    {
+        private readonly string FileName = "Establecimientos.txt";
+        public void Guardar(Establecimiento establecimiento)
+        {
+            FileStream file = new FileStream(FileName, FileMode.Append);
+            StreamWriter writer = new StreamWriter(file);
+            writer.WriteLine($"{establecimiento.nombre};{establecimiento.identificacion};{establecimiento.valorIngresosAnuales};{establecimiento.valorGastosAnuales};{establecimiento.tiempoFuncionamiento};{establecimiento.tipoResponsabilidad};{establecimiento.gananciasPesos};{establecimiento.gananciasUVT};{establecimiento.valorImpuesto} ");
+            writer.Close();
+            file.Close();
+
+        }
+        public List<Establecimiento> ConsultarTodos()
+        {
+            List<Establecimiento> establecimientos = new List<Establecimiento>();
+            FileStream file = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader reader = new StreamReader(file);
+            string linea = string.Empty;
+            while ((linea = reader.ReadLine()) != null)
+            {
+
+                Establecimiento establecimiento = Map(linea);
+                establecimientos.Add(establecimiento);
+            }
+            reader.Close();
+            file.Close();
+            return establecimientos;
+        }
+        private Establecimiento Map(string linea)
+        {
+            Establecimiento establecimiento = new Establecimiento();
+            char delimiter = ';';
+            string[] matrizPersona = linea.Split(delimiter);
+            establecimiento.nombre = matrizPersona[0];
+            establecimiento.identificacion = matrizPersona[1];
+            establecimiento.valorIngresosAnuales = Convert.ToDouble(matrizPersona[2]);
+            establecimiento.valorGastosAnuales = Convert.ToDouble(matrizPersona[3]);
+            establecimiento.tiempoFuncionamiento = int.Parse(matrizPersona[4]);
+            establecimiento.tipoResponsabilidad = matrizPersona[5];
+
+            return establecimiento;
+        }
+        private bool EsEncontrado(string identificacioRegistrada, string identificacionBuscada)
+        {
+            return identificacioRegistrada == identificacionBuscada;
+        }
+        public void Eliminar(string identificacion)
+        {
+            List<Establecimiento> establecimientos = new List<Establecimiento>();
+            establecimientos = ConsultarTodos();
+            FileStream file = new FileStream(FileName, FileMode.Create);
+            file.Close();
+            foreach (var item in establecimientos)
+            {
+                if (!EsEncontrado(item.identificacion, identificacion))
+                {
+                    Guardar(item);
+                }
+
+            }
+
+        }
+        public Establecimiento Buscar(string identificacion)
+        {
+            List<Establecimiento> establecimientos = ConsultarTodos();
+            foreach (var item in establecimientos)
+            {
+                if (EsEncontrado(item.identificacion, identificacion))
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+    }
+}
